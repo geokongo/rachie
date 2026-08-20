631247f8b1c4ac231a8534dbace8bcbf6f4789c9
Geoffrey <code@geokongo.com>
2026-08-20 11:54:55 -0400

    Load styles.css using Url::assets()

--- application/views/home/layout.php
+++ application/views/home/layout.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="icon" type="image/png" href="{{ Url::assets('images/Rachie Logo.png') }}">
-	<link rel="stylesheet" type="text/css" href="styles/main.css">
+	<link rel="stylesheet" type="text/css" href="{{ Url::assets('styles/main.css') }}">
 	<title>{{ $title }}</title>
 </head>
 <body>
